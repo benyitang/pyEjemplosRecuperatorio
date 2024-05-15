129b21e6eceabfabc8f92b405bf82ce9b3afcf54
benyitang <163897566+benyitang@users.noreply.github.com>
2024-05-15 15:30:13 -0300

    Create ej8.py

---
+++ ej8.py
@@ -0,0 +1,10 @@
+def ej8():
+    n1 = float(input("Ingrese un numero entero "))
+    n2 = float(input("Ingrese otro numero entero  "))
+
+    porcentaje = n2 / 100
+    total = n1 / porcentaje
+     
+    print("El resultado es: ", total)
+
+ej8()
