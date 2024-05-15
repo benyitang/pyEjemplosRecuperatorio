345ea77602a6d7cd947ff5b2e1f122a2fdb068a5
benyitang <163897566+benyitang@users.noreply.github.com>
2024-05-15 15:29:59 -0300

    Create ej4.py

---
+++ ej4.py
@@ -0,0 +1,7 @@
+def ej4():
+    ladon1 = float(input("Ingrese un numero decimal para calcular la superficie de un cuadrado"))
+    resultado = ladon1 ** 2
+     
+    print("El resultado es: ", resultado)
+
+ej4()
