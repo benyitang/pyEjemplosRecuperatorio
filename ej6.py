2a9a506ac2c8fb212eaa2a098086e9ab57303a66
benyitang <163897566+benyitang@users.noreply.github.com>
2024-05-15 15:30:07 -0300

    Create ej6.py

---
+++ ej6.py
@@ -0,0 +1,9 @@
+def ej6():
+    ladoAn1 = float(input("Ingrese un numero entero para calcular la superficie de un triangulo"))
+    ladoBn2 = float(input("Ingrese otro numero entero para calcular la superficie de un triangulo"))
+
+    resultado = ladoAn1 * ladoBn2 / 2
+     
+    print("El la superfice de su triangulo es: ", resultado)
+
+ej6()
