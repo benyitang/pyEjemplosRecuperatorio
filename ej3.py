b03d7e83ae33e0156438f07c2aa80dcb6d669b3d
benyitang <163897566+benyitang@users.noreply.github.com>
2024-05-15 15:29:56 -0300

    Create ej3.py

---
+++ ej3.py
@@ -0,0 +1,9 @@
+def ej3():
+    ladon1 = int(input("Ingrese un numero entero para calcular la superficie de un rectangulo"))
+    ladon2 = int(input("Ingrese otro numero entero para calcular la superficie de un rectangulo"))
+
+    resultado = ladon1 * ladon2
+     
+    print("El resultado es: ", resultado)
+
+ej3()
