3264ff35a4381c03e6755768968cd0d5b81daaa3
benyitang <163897566+benyitang@users.noreply.github.com>
2024-05-15 15:29:53 -0300

    Create ej2.py

---
+++ ej2.py
@@ -0,0 +1,11 @@
+def ej2():
+    n1 = int(input("Ingrese un numero entero"))
+    n2 = int(input("Ingrese otro numero entero"))
+    n3 = int(input("Ingrese otro numero entero"))
+    n4 = int(input("Ingrese otro numero entero"))
+
+    resultado = n1 + n2
+     
+    print("El resultado es: ", resultado)
+
+ej2()
