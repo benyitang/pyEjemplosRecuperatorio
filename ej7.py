65a7e71f7b6f376941cc96f291931fc7b3e45b94
benyitang <163897566+benyitang@users.noreply.github.com>
2024-05-15 15:30:09 -0300

    Create ej7.py

---
+++ ej7.py
@@ -0,0 +1,15 @@
+def ej7():
+    n1 = int(input("Ingrese un numero "))
+    n2 = int(input("Ingrese otro numero entero"))
+    n3 = int(input("Ingrese otro numero entero"))
+    n4 = int(input("Ingrese otro numero entero"))
+    n5 = int(input("Ingrese otro numero entero"))
+    n6 = int(input("Ingrese otro numero entero"))
+    n7 = int(input("Ingrese otro numero entero"))
+
+    total = n1 + n2 + n3 + n4 + n5 + n6 + n7
+    promedio = total / 6
+     
+    print("El resultado es: ", promedio)
+
+ej7()
